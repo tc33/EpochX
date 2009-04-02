fa82e1201083b4f44dc6c9a502efb2fda9d15948
tc33 <tc33@13a80019-f18c-62f3-d07f-c73cdeab6a78>
2009-04-02 18:25:40 +0000

    Implemented some basic boolean functions XOR, IFF etc

---
+++ src/com/epochx/func/bool/NorFunction.java
@@ -0,0 +1,54 @@
+/*  
+ *  Copyright 2007-2008 Lawrence Beadle & Tom Castle
+ *  Licensed under GNU General Public License
+ * 
+ *  This file is part of Epoch X - (The Genetic Programming Analysis Software)
+ *
+ *  Epoch X is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Epoch X is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with Epoch X.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.epochx.func.bool;
+
+import com.epochx.core.representation.*;
+
+/**
+ * 
+ */
+public class NorFunction extends FunctionNode<Boolean> {
+	
+	public NorFunction() {
+		this(null, null);
+	}
+	
+	public NorFunction(Node<Boolean> child1, Node<Boolean> child2) {
+		super(child1, child2);
+	}
+	
+	@Override
+	public Boolean evaluate() {
+		boolean c1 = ((Boolean) getChild(0).evaluate()).booleanValue();
+		boolean c2 = ((Boolean) getChild(1).evaluate()).booleanValue();
+		
+		return !(c1 || c2);
+	}
+	
+	@Override
+	public String getFunctionName() {
+		return "NOR";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj) && (obj instanceof NorFunction);
+	}
+}

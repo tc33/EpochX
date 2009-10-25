45a77d41e16f15f3aafd3931d721b11a6f377862
tc33 <tc33@13a80019-f18c-62f3-d07f-c73cdeab6a78>
2009-10-25 15:22:28 +0000

    Removed dependence upon external file in SantaFeTrail model.

--- models-src/com/epochx/model/ant/SantaFeTrail.java
+++ models-src/com/epochx/model/ant/SantaFeTrail.java
@@ -20,7 +20,6 @@
 package com.epochx.model.ant;
 
 import java.awt.*;
-import java.io.*;
 import java.util.*;
 import java.util.List;
 
@@ -29,37 +28,46 @@ import com.epochx.ant.*;
 import com.epochx.core.*;
 import com.epochx.representation.*;
 import com.epochx.representation.action.*;
-import com.epochx.util.*;
+import com.epochx.stats.GenerationStatField;
 
 /**
  * 
  */
 public class SantaFeTrail extends GPAbstractModel<Action> {
 
-	private List<String> inputs = new ArrayList<String>();;
-	private HashMap<String, TerminalNode<Action>> variables = new HashMap<String, TerminalNode<Action>>();
-	private Dimension dimension = new Dimension(32, 32);	
-	private ArrayList<Point> foodLocations = new ArrayList<Point>();
-	private AntLandscape antLandscape = new AntLandscape(dimension, null);
-	private Ant ant = new Ant(600, antLandscape);
-	private int noOfFoodPellets;
-	private int fitAssessed = 0;
+	private HashMap<String, TerminalNode<Action>> variables = new HashMap<String, TerminalNode<Action>>();	
+	private AntLandscape landscape;
+	private Ant ant;
 	
-	public SantaFeTrail() {		
-		inputs = FileManip.loadInput(new File("inputsantafe.txt"));
-		// create list of food locations
-		foodLocations = new ArrayList<Point>();
-		for(String i: inputs) {
-			if(!i.equalsIgnoreCase("DC")) {
-				String[] parts = i.split(":");
-				int x = Integer.parseInt(parts[0]);
-				int y = Integer.parseInt(parts[1]);
-				Point p = new Point(x, y);
-				foodLocations.add(p);
-			}
-		}
-		
-		noOfFoodPellets = foodLocations.size();
+	private static final Point[] foodLocations = {
+		new Point(1,0), new Point(2,0), new Point(3,0), new Point(3,1),
+		new Point(3,2), new Point(3,3), new Point(3,4), new Point(3,5),
+		new Point(4,5), new Point(5,5), new Point(6,5), new Point(8,5),
+		new Point(9,5), new Point(10,5), new Point(11,5), new Point(12,5),
+		new Point(12,6), new Point(12,7), new Point(12,8), new Point(12,9),
+		new Point(12,11), new Point(12,12), new Point(12,13), new Point(12,14),
+		new Point(12,17), new Point(12,18), new Point(12,19), new Point(12,20),
+		new Point(12,21), new Point(12,22), new Point(12,23), new Point(11,24),
+		new Point(10,24), new Point(9,24), new Point(8,24), new Point(7,24),
+		new Point(4,24), new Point(3,24), new Point(1,25), new Point(1,26),
+		new Point(1,27), new Point(1,28), new Point(2,30), new Point(3,30),
+		new Point(4,30), new Point(5,30), new Point(7,29), new Point(7,28),
+		new Point(8,27), new Point(9,27), new Point(10,27), new Point(11,27),
+		new Point(12,27), new Point(13,27), new Point(14,27), new Point(16,26),
+		new Point(16,25), new Point(16,24), new Point(16,21), new Point(16,20),
+		new Point(16,19), new Point(16,18), new Point(17,15), new Point(20,14),
+		new Point(20,13), new Point(20,10), new Point(20,9), new Point(20,8),
+		new Point(20,7), new Point(21,5), new Point(22,5), new Point(24,4),
+		new Point(24,3), new Point(25,2), new Point(26,2), new Point(27,2),
+		new Point(29,3), new Point(29,4), new Point(29,6), new Point(29,9),
+		new Point(29,12), new Point(28,14), new Point(27,14), new Point(26,14),
+		new Point(23,15), new Point(24,18), new Point(27,19), new Point(26,22),
+		new Point(23,23)
+	};
+	
+	public SantaFeTrail() {
+		landscape = new AntLandscape(new Dimension(32, 32), null);
+		ant = new Ant(600, landscape);
 		
 		configure();
 	}
@@ -75,7 +83,7 @@ public class SantaFeTrail extends GPAbstractModel<Action> {
 	public List<FunctionNode<Action>> getFunctions() {
 		// Define functions.
 		List<FunctionNode<Action>> functions = new ArrayList<FunctionNode<Action>>();
-		functions.add(new IfFoodAheadFunction(ant, antLandscape));
+		functions.add(new IfFoodAheadFunction(ant, landscape));
 		functions.add(new Seq2Function());
 		functions.add(new Seq3Function());
 		return functions;
@@ -91,32 +99,19 @@ public class SantaFeTrail extends GPAbstractModel<Action> {
 		return terminals;
 	}
 	
-	/* (non-Javadoc)
-	 * @see com.epochx.core.GPModel#getFitness(com.epochx.core.representation.CandidateProgram)
-	 */
 	@Override
-	public double getFitness(CandidateProgram<Action> program) {
-		fitAssessed++;
-		//System.out.println(program.getRootNode());
-		//System.out.println(fitAssessed);
-		// refresh food list before evaluation
-		// hard copy foodLocations
-		ArrayList<Point> fl = new ArrayList<Point>();
-		for(Point p: foodLocations) {
-			fl.add(p);
-		}
-		//System.out.println(fl);
-		antLandscape.setFoodLocations(fl);
-		ant.reset(600, antLandscape);
-		//System.out.println("A1 = " + ant);
-		// run the ant
-		while(ant.getMoves()<ant.getMaxMoves()) {
+	public double getFitness(CandidateProgram<Action> program) {		
+		landscape.setFoodLocations(new ArrayList<Point>(Arrays.asList(foodLocations)));
+		ant.reset(600, landscape);
+
+		// Run the ant.
+		while(ant.getMoves() < ant.getMaxMoves()) {
 			program.evaluate();
 		}
-		//System.out.println("A2 = " + ant);
-		// calculate score
-		double score = (double) (noOfFoodPellets - ant.getFoodEaten());
-		//System.out.println(score);
+
+		// Calculate score.
+		double score = (double) (foodLocations.length - ant.getFoodEaten());
+
 		return score;
 	}
 	
@@ -125,6 +120,12 @@ public class SantaFeTrail extends GPAbstractModel<Action> {
 	}
 	
 	public AntLandscape getAntLandScape() {
-		return antLandscape;
+		return landscape;
+	}
+	
+	public static void main(String[] args) {
+		GPAbstractModel<Action> model = new SantaFeTrail();
+		model.setGenStatFields(new GenerationStatField[]{GenerationStatField.FITNESS_MIN});
+		GPController.run(model);
 	}
 }

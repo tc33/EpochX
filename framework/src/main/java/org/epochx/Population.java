e1316d1d4478e19900450452a3714573dcc4da67
Fernando Otero <fernando.otero@mac.com>
2011-05-20 18:56:38 +0100

    Added method to return the fittest individual.
    Change-Id: If3b9faf9c6d04a1c666d954ff335fc88dbdf1668

--- framework/src/main/java/org/epochx/Population.java
+++ framework/src/main/java/org/epochx/Population.java
@@ -7,33 +7,45 @@ package org.epochx;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.epochx.Config.*;
+import org.epochx.Config.ConfigKey;
 
 /**
+ * TODO: make it iterable
+ * 
  * @author Fernando Esteban Barril Otero
  * @version $Revision: 615 $ $Date:: 2011-04-14 12:27:06#$
  */
-public class Population
-{
-    public static final ConfigKey<Integer> SIZE = new ConfigKey<Integer>();
+public class Population {
+	public static final ConfigKey<Integer> SIZE = new ConfigKey<Integer>();
 
-    private List<Individual> individuals;
+	private List<Individual> individuals;
 
-    public Population()
-    {
-        individuals = new ArrayList<Individual>(Config.getInstance().get(SIZE));
-    }
-
-    public int size()
-    {
-        return individuals.size();
-    }
-    
-    public void add(Individual individual) {
-    	individuals.add(individual);
+	public Population() {
+		individuals = new ArrayList<Individual>(Config.getInstance().get(SIZE));
 	}
-    
-    public Individual get(int index) {
-    	return individuals.get(index);
-    }
+
+	public int size() {
+		return individuals.size();
+	}
+
+	public void add(Individual individual) {
+		individuals.add(individual);
+	}
+
+	public Individual get(int index) {
+		return individuals.get(index);
+	}
+
+	public Individual fittest() {
+		Individual fittest = null;
+
+		for (Individual individual : individuals) {
+			if (fittest == null
+					|| individual.getFitness().compareTo(fittest.getFitness()) > 0) {
+				fittest = individual;
+			}
+		}
+
+		return fittest;
+	}
 }

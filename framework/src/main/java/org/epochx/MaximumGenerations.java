14ba6ea4626833e6cdcb475d32a01a0f1216c22c
Fernando Otero <fernando.otero@mac.com>
2011-05-20 18:55:35 +0100

    Now listening to config events.
    Change-Id: Id90c081d83bb6410205c5623dae55347b10e8852

--- framework/src/main/java/org/epochx/MaximumGenerations.java
+++ framework/src/main/java/org/epochx/MaximumGenerations.java
@@ -1,15 +1,20 @@
 package org.epochx;
 
-import org.epochx.Config.*;
-import org.epochx.event.*;
-import org.epochx.event.GenerationEvent.*;
+import org.epochx.Config.ConfigKey;
+import org.epochx.event.EventManager;
+import org.epochx.event.GenerationEvent.EndGeneration;
+import org.epochx.event.Listener;
 
 public class MaximumGenerations implements TerminationCriteria,
 		Listener<EndGeneration> {
 
 	public static final ConfigKey<Integer> MAXIMUM_GENERATIONS = new ConfigKey<Integer>();
-	
+
 	private int generation = 0;
+
+	public MaximumGenerations() {
+		EventManager.getInstance().add(EndGeneration.class, this);
+	}
 
 	public boolean terminate() {
 		return generation >= Config.getInstance().get(MAXIMUM_GENERATIONS);

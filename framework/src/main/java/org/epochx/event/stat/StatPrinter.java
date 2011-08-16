77b23c1805499069304b1c4c38fa76d934c85592
Fernando Otero <fernando.otero@mac.com>
2011-08-16 11:26:38 +0100

    Initial implementation of the stat printer.

---
+++ framework/src/main/java/org/epochx/event/stat/StatPrinter.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2007-2011
+ * Lawrence Beadle, Tom Castle and Fernando Otero
+ * Licensed under GNU Lesser General Public License
+ * 
+ * This file is part of EpochX
+ * 
+ * EpochX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EpochX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with EpochX. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The latest version is available from: http:/www.epochx.org
+ */
+
+package org.epochx.event.stat;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import org.epochx.event.Event;
+import org.epochx.event.EventManager;
+import org.epochx.event.Listener;
+
+/**
+ * 
+ */
+public class StatPrinter {
+
+	/**
+	 * The default separator string.
+	 */
+	public static final String SEPARATOR = "\t";
+
+	/**
+	 * The list of <code>AbstractStat</code> to be printed.
+	 */
+	private ArrayList<AbstractStat<?>> fields = new ArrayList<AbstractStat<?>>();
+
+	/**
+	 * The current separator.
+	 */
+	private String separator;
+
+	/**
+	 * The output stream.
+	 */
+	private PrintStream out;
+
+	/**
+	 * Constructs a <code>StatPrinter</code>.
+	 */
+	public StatPrinter() {
+		this(System.out, SEPARATOR);
+	}
+
+	/**
+	 * Constructs a <code>StatPrinter</code>.
+	 * 
+	 * @param out the output stream.
+	 */
+	public StatPrinter(PrintStream out) {
+		this(out, SEPARATOR);
+	}
+
+	/**
+	 * Constructs a <code>StatPrinter</code>.
+	 * 
+	 * @param separator the delimiter string.
+	 */
+	public StatPrinter(String separator) {
+		this(System.out, separator);
+	}
+
+	/**
+	 * Constructs a <code>StatPrinter</code>.
+	 * 
+	 * @param out the output stream.
+	 * @param delimiter the delimiter string.
+	 */
+	public StatPrinter(PrintStream out, String separator) {
+		this.out = out;
+		this.separator = separator;
+
+	}
+
+	public <E extends Event> void add(Class<? extends AbstractStat<E>> type) {
+		AbstractStat.register(type);
+		fields.add(AbstractStat.get(type));
+	}
+
+	public void clear() {
+		fields.clear();
+	}
+
+	public void print() {
+		if (!fields.isEmpty()) {
+			StringBuffer buffer = new StringBuffer();
+
+			for (AbstractStat<?> stat: fields) {
+				buffer.append(separator);
+				buffer.append(stat);
+			}
+
+			buffer.delete(0, separator.length());
+			out.println(buffer.toString());
+		}
+	}
+
+	public <E extends Event> void printOnEvent(Class<E> type) {
+		EventManager.getInstance().add(type, new Listener<E>() {
+
+			public void onEvent(E event) {
+				StatPrinter.this.print();
+			}
+		});
+	}
+
+}

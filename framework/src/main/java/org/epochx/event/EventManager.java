aca690cd4d9729295aab0a8289d4111f7ebb9b83
febo <fernando.otero@mac.com>
2011-09-27 16:28:12 +0100

    Javadoc and added a method to reset the listener mapping.

--- framework/src/main/java/org/epochx/event/EventManager.java
+++ framework/src/main/java/org/epochx/event/EventManager.java
@@ -28,26 +28,49 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Not thread-safe.
+ * The <code>EventManager</code> class provides event-related functionality.
+ * It is a singleton, which is obtainable with the <code>getInstance</code>
+ * method. It provides methods for registering listeners and firing events.
+ * 
+ * <p>
+ * <b>Note:</b> The current implementation is not thread-safe.
+ * </p>
+ * 
+ * @see Listener
+ * @see Event
  */
 public class EventManager {
 
+	/**
+	 * The singleton instance.
+	 */
 	private static final EventManager singleton = new EventManager();
 
+	/**
+	 * The mapping of listeners per event.
+	 */
 	private final HashMap<Class<?>, List<Listener<?>>> mapping = new HashMap<Class<?>, List<Listener<?>>>();
 
+	/**
+	 * Constructs a <code>EventManager</code>.
+	 */
 	private EventManager() {
 	}
 
+	/**
+	 * Returns the singleton instance.
+	 * 
+	 * @return the singleton instance.
+	 */
 	public static EventManager getInstance() {
 		return singleton;
 	}
 
 	/**
+	 * Registers a listener for the specified event.
 	 * 
-	 * @param <T>
-	 * @param key the class of the event
-	 * @param listener
+	 * @param key the class of the event.
+	 * @param listener the listener object.
 	 */
 	public <T extends Event> void add(Class<? extends T> key, Listener<T> listener) {
 
@@ -59,19 +82,26 @@ public class EventManager {
 	}
 
 	/**
-	 * Removing a listener that has been added more than once will remove the
-	 * only instance
-	 * of the listener and so it will cease to listen for all parties.
+	 * Removes a listener from the specified event. This effectively makes the
+	 * listener stop receiveing notifications of the event.
 	 * 
-	 * @param <T>
-	 * @param key
-	 * @param listener
-	 * @return
+	 * @param key the class of the event.
+	 * @param listener the listener object.
+	 * 
+	 * @return <code>true</code> if the event's listener mapping contained the
+	 *         specified listener.
 	 */
 	public <T extends Event> boolean remove(Class<? extends T> key, Listener<T> listener) {
-		return mapping.get(key).remove(listener);
+		List<Listener<?>> listeners = mapping.get(key);
+		return listeners == null ? false : listeners.remove(listener);
 	}
 
+	/**
+	 * Fires the specified event by notifying all registered listeners.
+	 * 
+	 * @param type the class of the event.
+	 * @param event the event object.
+	 */
 	@SuppressWarnings("unchecked")
 	public <T extends Event, V extends T> void fire(Class<V> type, T event) {
 		for (Class<?> key: mapping.keySet()) {
@@ -81,6 +111,14 @@ public class EventManager {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Removes all events listener mapping. The <code>EventManager</code> will
+	 * be empty this call returns.
+	 */
+	public void reset() {
+		mapping.clear();
 	}
 
 }

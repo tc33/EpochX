18db3337e7eacf5753f33c44e1862d918b2ff04c
tc33 <tom@tc33.org>
2014-08-18 23:52:24 +0100

    Tidied GE Chromosome class

--- ge/src/main/java/org/epochx/ge/Chromosome.java
+++ ge/src/main/java/org/epochx/ge/Chromosome.java
@@ -31,9 +31,14 @@ import java.util.*;
 import org.epochx.Config.ConfigKey;
 import org.epochx.*;
 import org.epochx.event.*;
+import org.epochx.grammar.Grammar;
 
 /**
- * A chromosome is a sequential, iterable list of Codons. 
+ * A chromosome is a sequential, iterable list of {@link Codon}s, which are used to represent
+ * an individual solution in Grammatical Evolution. Each codon has a numeric value, but otherwise
+ * may be represented in any format, for example, as an integer or a binary string. In the standard
+ * Grammatical Evolution algorithm, an individual's chromosome is used by a {@link Mapper} to 
+ * generate a parse tree with reference to a problem specific {@link Grammar}.
  */
 public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEvent> {
 
@@ -49,7 +54,7 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 	
 	/**
 	 * The key for setting and retrieving whether chromosomes should be allowed to 
-	 * themselves extend up to the maximum length setting if there are insufficient
+	 * extend themselves up to the maximum length setting if there are insufficient
 	 * codons
 	 */
 	public static final ConfigKey<Boolean> ALLOW_EXTENSION = new ConfigKey<Boolean>();
@@ -171,11 +176,20 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 		return new ArrayList<Codon>(codons);
 	}
 	
-	
+	/**
+	 * Returns the codon at the given index. If the index is beyond the length of the chromosome and 
+	 * extending is enabled then the chromosome will be extended up to the index, or to the maximum
+	 * length setting, whichever is smaller. If the index is still not within the length of the 
+	 * chromosome and wrapping is enabled then the codons will be wrapped up to the maximum wraps 
+	 * setting. If the index is not reachable even with wrapping and extending then null is returned.
+	 * 
+	 * @param index the codon to return
+	 * @return the codon at the specified index
+	 */
 	public Codon getCodon(long index) {
 		// If within chromosome size just return the codon
 		if (index < codons.size()) {
-			return codons.get((int) index);		
+			return codons.get((int) index);
 		}
 		
 		// Otherwise need to do some combination of wrapping and extending
@@ -200,18 +214,46 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 		return codons.get((int) index);
 	}
 
+	/**
+	 * Replaces the codon at the specified index. Wrapping and extending are not used, so the
+	 * maximum index is the number of codons minus one.
+	 * 
+	 * @param index the index of the codon to replace
+	 * @param codon the codon to set
+	 */
 	public void setCodon(int index, Codon codon) {
 		codons.set(index, codon);
 	}
 
+	/**
+	 * Returns the number of codons in the chromosome
+	 * 
+	 * @return the size of the chromosome
+	 */
 	public int length() {
 		return codons.size();
 	}
 
+	/**
+	 * Removes and returns the codon at the specified index. The index position will 
+	 * not be wrapped and codon length will not be extended.
+	 * 
+	 * @param index the index of the codon to remove
+	 * @return the codon that was removed
+	 */
 	public Codon removeCodon(int index) {
 		return codons.remove(index);
 	}
 
+	/**
+	 * Removes a range of codons between the given indices and returns a list of the
+	 * codons that are removed. The index positions will not be wrapped and codon length 
+	 * will not be extended.
+	 * 
+	 * @param from the starting index (inclusive)
+	 * @param to the upper index (exclusive)
+	 * @return a list of the codons that were removed
+	 */
 	public List<Codon> removeCodons(int from, int to) {
 		List<Codon> removed = new ArrayList<Codon>();
 		for (int i = from; i < to; i++) {
@@ -222,31 +264,140 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 		return removed;
 	}
 
+	/**
+	 * Appends the given codon to the end of the chromosome
+	 * 
+	 * @param codon the codon to append
+	 */
 	public void appendCodon(Codon codon) {
 		codons.add(codon);
 	}
 	
+	/**
+	 * Appends multiple codons to the end of the chromosome
+	 * 
+	 * @param codons a list of codons to append
+	 */
 	public void appendCodons(List<Codon> codons) {
 		codons.addAll(codons);
 	}
 
-
+	/**
+	 * Returns the maximum number of wraps allowed
+	 * 
+	 * @return the maximum wraps setting
+	 */
 	public int getMaxWraps() {
 		return maxWraps;
 	}
 
+	/**
+	 * Sets the maximum number of times the codons can be wrapped. If automatic configuration 
+	 * is enabled then any value set here will be overwritten by the 
+	 * {@link Chromosome#MAXIMUM_WRAPS} configuration setting on the next config event.
+	 * 
+	 * @param maxWraps the maximum number of times to allow the codons to wrap
+	 */
 	public void setMaxWraps(int maxWraps) {
 		this.maxWraps = maxWraps;
 	}
 
+	/**
+	 * Returns true if auto-extending is enabled and false otherwise
+	 * 
+	 * @return true if auto-extending is enabled and false otherwise
+	 */
 	public boolean isAutoExtending() {
 		return extending;
 	}
 
+	/**
+	 * Enables or disables auto-extending of the chromosome. If automatic configuration is 
+	 * enabled then any value set here will be overwritten by the 
+	 * {@link Chromosome#ALLOW_EXTENSION} configuration setting on the next config event.
+	 * 
+	 * @param extending true if auto-extending should be enabled or false to disable it
+	 */
 	public void setAutoExtending(boolean extending) {
 		this.extending = extending;
 	}
+	
+	/**
+	 * @return the random
+	 */
+	public RandomSequence getRandom() {
+		return random;
+	}
 
+	/**
+	 * @param random the random to set
+	 */
+	public void setRandom(RandomSequence random) {
+		this.random = random;
+	}
+
+	/**
+	 * @return the minCodon
+	 */
+	public Long getMinCodon() {
+		return minCodon;
+	}
+
+	/**
+	 * @param minCodon the minCodon to set
+	 */
+	public void setMinCodon(Long minCodon) {
+		this.minCodon = minCodon;
+	}
+
+	/**
+	 * @return the maxCodon
+	 */
+	public Long getMaxCodon() {
+		return maxCodon;
+	}
+
+	/**
+	 * @param maxCodon the maxCodon to set
+	 */
+	public void setMaxCodon(Long maxCodon) {
+		this.maxCodon = maxCodon;
+	}
+
+	/**
+	 * @return the maxLength
+	 */
+	public Integer getMaxLength() {
+		return maxLength;
+	}
+
+	/**
+	 * @param maxLength the maxLength to set
+	 */
+	public void setMaxLength(Integer maxLength) {
+		this.maxLength = maxLength;
+	}
+
+	/**
+	 * @return the codonFactory
+	 */
+	public CodonFactory getCodonFactory() {
+		return codonFactory;
+	}
+
+	/**
+	 * @param codonFactory the codonFactory to set
+	 */
+	public void setCodonFactory(CodonFactory codonFactory) {
+		this.codonFactory = codonFactory;
+	}
+
+
+	/**
+	 * Creates and returns a clone of this chromosome. The internal list of codons is cloned.
+	 * 
+	 * @return a copy of this chromosome
+	 */
 	@Override
 	public Chromosome clone() {
 		Chromosome clone = null;
@@ -261,11 +412,24 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 		return clone;
 	}
 
+	/**
+	 * Returns a string representation of this chromosome's list of codons
+	 * 
+	 * @return a string representation of the list of codons
+	 */
 	@Override
 	public String toString() {
 		return codons.toString();
 	}
 
+	/**
+	 * Compares the given object to this instance for equality. Equivalence is
+	 * defined as them both being instances of <code>Chromosome</code> and
+	 * having equal lists of codons. The two chromosomes must also have the same
+	 * settings for allowing extending, maximum wraps and maximum length.
+	 * 
+	 * @return true if the object is an equivalent chromosome, false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Chromosome) {
@@ -290,19 +454,29 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 		return new ChromosomeIterator();
 	}
 	
+	/*
+	 * An iterator for looping through a chromosome's codons
+	 */
 	private class ChromosomeIterator implements Iterator<Codon> {
 
 		private Codon previous;
 		private long nextPosition;
 		
+		/**
+		 * Constructs a <code>ChromosomeIterator</code> with the current position set
+		 * as the first codon in the chromosome
+		 */
 		public ChromosomeIterator() {
 			nextPosition = 0;
 			previous = null;
 		}
 
 		/**
-		 * Note that if extending and wrapping are used, hasNext will be inconsistent
-		 * with the length of the underlying chromosome
+		 * Returns true if the current position is not beyond the end of the chromosome. If extending 
+		 * or wrapping are enabled then it will make use of them and therefore will be inconsistent 
+		 * with the length of the underlying chromosome.
+		 * 
+		 * @return true if a codon is available at the current position and false otherwise
 		 */
 		@Override
 		public boolean hasNext() {			
@@ -321,6 +495,12 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 			return (nextPosition <= lastPosition);
 		}
 
+		/**
+		 * Returns the next codon and increments the index of the current codon
+		 * 
+		 * @return the next codon if there is one
+		 * @throws NoSuchElementException if there is not a next codon 
+		 */
 		@Override
 		public Codon next() {
 			if (hasNext()) {
@@ -331,13 +511,20 @@ public class Chromosome implements Iterable<Codon>, Cloneable, Listener<ConfigEv
 			}
 		}
 
+		/**
+		 * Removes the current codon from the chromosome
+		 * 
+		 * @throws NoSuchElementException if there is no codon to remove, such as if the 
+		 * chromosome is empty or if the <code>next()</code> method has not been called
+		 * yet
+		 */
 		@Override
 		public void remove() {
-			if (previous == null) {
+			if (previous != null) {
 				codons.remove(nextPosition-1);
 				previous = null;
 			} else {
-				throw new IllegalStateException("No previous codon to remove");
+				throw new NoSuchElementException("No current codon to remove");
 			}
 		}
 	}
